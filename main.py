690a362d4ad2be379e7d7e01f523e1c0495dd2a9
tjdxo <rlatjdxo951@gmail.com>
2026-08-06 20:31:52 +0900

    Feat: Quiz 클래스 구현

--- main.py
+++ main.py
@@ -26,3 +26,17 @@ while True:
         break
     else:
         print("잘못된 입력입니다.")
+
+class Quiz:
+    def __init__(self, question, choices, answer):
+        self.question = question
+        self.choices = choices
+        self.answer = answer
+
+    def display(self):
+        print(self.question)
+        for i, choice in enumerate(self.choices, start=1):
+            print(f"{i}. {choice}")
+
+    def check_answer(self, user_answer):
+        return self.answer == user_answer
